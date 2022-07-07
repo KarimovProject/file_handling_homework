84fc500ef3fb96fca895374b929a72d4f2d4d21a
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-07 19:54:59 -0400

    8-misol

--- file08.py
+++ file08.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    x = []
+    i = 0
+    while i <len(data):
+        if data[i].isdigit():
+            x.append(int(data[i]))
+        i+=1
+    return max(x)
 # Read data from file
+data = open('txt_file/data08.txt')
+x = data.read()
+print(main(x))
