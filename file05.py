1075161acf3044ee406e44921d5a460932da5fdd
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-07 19:16:39 -0400

    5-misol

--- file05.py
+++ file05.py
@@ -1,3 +1,6 @@
+from re import S
+
+
 def main(data:str):
     """
     The data is from the file. Find the number of digital and str(non-digital) data and return as list type.
@@ -7,4 +10,18 @@ def main(data:str):
         list: return answer
     """
     
+    c = 0
+    s = 0
+    l = [c,s]
+    for i in data:
+        if i.isdigit():
+            c+=1
+        elif not(i.isdigit()):
+            s += 1
+        l = [c,s]
+    return l
+    
 # Read data from file
+file = open('txt_file/data05.txt','r')
+data = file.read()
+print(main(data))
