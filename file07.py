6dfa4a2613436f0a59b6113c6975903a2cb983ee
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-07 19:53:18 -0400

    7-misol

--- file07.py
+++ file07.py
@@ -6,5 +6,14 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    x = []
+    i = 0
+    while i <len(data):
+        if data[i].isdigit():
+            x.append(int(data[i]))
+        i+=1
+    return sum(x)
 # Read data from file
+data = open('txt_file/data07.txt')
+x = data.read()
+print(main(x))
