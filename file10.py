a1199c02574569573a007796b51efe6c705903bd
KarimovProject <abdullatifkarimov772@gmail.com>
2022-07-07 20:03:33 -0400

    10-misol

--- file10.py
+++ file10.py
@@ -6,5 +6,15 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    x = data.split('\n')
+    s = []
+    i = 0
+    while i < len(x):
+        s.append(len(x[i]))
+        i += 1
 
+    return max(s)
+    
 # Read data from file
+f = open("txt_file/data10.txt").read()
+print(main(f))
